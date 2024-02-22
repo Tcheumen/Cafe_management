0d9de60023ee57d7cd132fb73287f489b2913c81
Tcheumen Lionel <tcheumenlionel@gmail.com>
2024-02-22 04:44:51 +0100

    add all api of product

--- CafeManagement/Backend/index.js
+++ CafeManagement/Backend/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 var cors = require('cors');
 const connection = require('./connection');
 const userRoute = require('./routes/user.js');
+const productRoute = require('./routes/product.js');
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/user', userRoute);
+app.use('/product', productRoute);
 
 module.exports = app;
