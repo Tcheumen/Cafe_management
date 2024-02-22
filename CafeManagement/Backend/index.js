e70c474c7ed284cde22c8770d6b91045c8da0714
Tcheumen Lionel <tcheumenlionel@gmail.com>
2024-02-22 02:14:50 +0100

    Api to add, get and update of Category

--- CafeManagement/Backend/index.js
+++ CafeManagement/Backend/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 var cors = require('cors');
 const connection = require('./connection');
 const userRoute = require('./routes/user.js');
+const categoryRoute = require('./routes/category.js');
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/user', userRoute);
+app.use('/category', categoryRoute);
 
 module.exports = app;
