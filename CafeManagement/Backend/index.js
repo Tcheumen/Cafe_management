538415387154b691abbc74b90ff5e15eb2380f94
Tcheumen Lionel <tcheumenlionel@gmail.com>
2024-02-27 02:37:10 +0100

    api to add  details api of dashboard

--- CafeManagement/Backend/index.js
+++ CafeManagement/Backend/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 var cors = require('cors');
 const connection = require('./connection');
 const userRoute = require('./routes/user.js');
+const dashboardRoute = require('./routes/dashboard.js');
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/user', userRoute);
+app.use('/dashboard', dashboardRoute);
 
 module.exports = app;
